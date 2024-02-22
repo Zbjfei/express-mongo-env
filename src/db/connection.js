2a5fc01af6eea44d8532a26d092b2b033765d515
zbjfei <zbjfei@qq.com>
2024-02-22 22:34:51 +0300

    commit to my resp

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,6 +1,7 @@
 const monk = require('monk');
 
-const dbUrl = process.env.DB_URL;
+// const dbUrl = process.env.DB_URL;
+const dbUrl = 'mongodb://zbjfei:fn9n@isit.my.to:27017/?authSource=WangzihuDB';
 
 // console.log(process.env.NODE_ENV);
 
